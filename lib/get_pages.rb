1436fb31064c9596813987d0270597ed1b1453af
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2011-05-25 15:26:35 -0400

    Updated script to collect and locally store chair and directorship information.

--- lib/get_pages.rb
+++ lib/get_pages.rb
@@ -1,6 +1,7 @@
 require 'rest_client'
 require 'json'
 require 'active_support/core_ext/hash'
+require 'yaml'
 
 def get_json_response_for(path)
   RestClient.get("http://#{File.join('professorships.nd.edu', path).sub(/\/$/, '')}.js", {:params => {'children' => 'true'}, :accept => :json})
@@ -14,6 +15,10 @@ end
 # store them in a more permanent location
 @chairs = {}
 @directorships = {}
+@colleges = {}
+parse_json_for('/professorships/colleges')['data'].each do |response|
+  @colleges[response['college']] = response['id']
+end
 
 # Build the initial collection of professorships.
 parse_json_for('/by-college')['children'].each do |college|
@@ -21,14 +26,19 @@ parse_json_for('/by-college')['children'].each do |college|
   college_name = college['name'].strip
 
   parse_json_for(college['permalink'])['children'].each do |professorship|
-    chair_name      = professorship['meta_attributes']['chair'].strip
-    prof_name       = professorship['meta_attributes']['name'].strip
-    prof_photo      = professorship['meta_attributes']['photo'].strip
-    prof_last_name  = professorship['meta_attributes']['lastnameprof'].strip
-    prof_biography  = professorship['meta_attributes']['bio'].strip
-    chair_last_name = professorship['meta_attributes']['lastnametitle'].strip
-    permalink       = professorship['permalink']
-    prof_first_name = prof_name.sub(prof_last_name, '').strip
+    chair_name       = professorship['meta_attributes']['chair'].strip
+    prof_name        = professorship['meta_attributes']['name'].strip
+    professorship['meta_attributes']['photo'].strip =~ /src="([^"]*)"/
+    prof_photo      = $1
+    prof_last_name   = professorship['meta_attributes']['lastnameprof'].strip
+    prof_biography   = professorship['meta_attributes']['bio'].strip
+    chair_last_name  = professorship['meta_attributes']['lastnametitle'].strip
+    permalink        = professorship['permalink']
+    prof_first_name  = prof_name.sub(prof_last_name, '').strip
+    college_id = nil
+    unless college_id = @colleges[college_name]
+      puts "Warning: no college for #{permalink}"
+    end
     @chairs[chair_name] ||= []
     @chairs[chair_name] << {
       'chair_name'      => chair_name,
@@ -36,9 +46,11 @@ parse_json_for('/by-college')['children'].each do |college|
       'prof_first_name' => prof_first_name,
       'prof_last_name'  => prof_last_name,
       'prof_biography'  => prof_biography,
-      'prof_photo'      => prof_photo,
+      'photo_upload'    => prof_photo,
       'chair_last_name' => chair_last_name,
       'permalink'       => permalink,
+      'college_name'    => college_name,
+      'college_id'      => college_id,
       'chair'           => true,
       'directorship'    => false,
     }
@@ -49,7 +61,8 @@ end
 parse_json_for('/directorships')['children'].each do |directorship|
   chair_name      = directorship['meta_attributes']['chair'].strip
   prof_name       = directorship['meta_attributes']['name'].strip
-  prof_photo      = directorship['meta_attributes']['photo'].strip
+  directorship['meta_attributes']['photo'].strip =~ /src="([^"]*)"/
+  prof_photo      = $1
   prof_last_name  = directorship['meta_attributes']['lastnameprof'].strip
   prof_biography  = directorship['meta_attributes']['bio'].strip
   chair_last_name = directorship['meta_attributes']['lastnametitle'].strip
@@ -63,16 +76,23 @@ parse_json_for('/directorships')['children'].each do |directorship|
     'prof_first_name' => prof_first_name,
     'prof_last_name'  => prof_last_name,
     'prof_biography'  => prof_biography,
-    'prof_photo'      => prof_photo,
+    'photo_upload'    => prof_photo,
     'chair_last_name' => chair_last_name,
     'permalink'       => permalink,
+    'college_id'      => nil,
     'chair'           => false,
     'directorship'    => true,
   }
 end
 
-# Looks like we have duplicate data to consider.  This is how we are going to
-# check
-@duplicate_chairs = @chairs.select {|k,v| v.size > 1}
-@duplicate_directorships = @directorships.select {|k,v| v.size > 1}
+File.open(File.join(File.dirname(__FILE__), '..','tmp/chairs.yml'), 'w+') do |file|
+  file.puts(YAML.dump(@chairs))
+end
 
+File.open(File.join(File.dirname(__FILE__), '..','tmp/colleges.yml'), 'w+') do |file|
+  file.puts(YAML.dump(@colleges))
+end
+
+File.open(File.join(File.dirname(__FILE__), '..','tmp/directorships.yml'), 'w+') do |file|
+  file.puts(YAML.dump(@directorships))
+end
