1295c504ccad5c9174491bb695f93deaed3bbf8f
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2011-07-27 11:17:47 -0400

    Reworking professorships naming convention.

--- lib/get_pages.rb
+++ lib/get_pages.rb
@@ -4,8 +4,15 @@ require 'active_support/core_ext/hash'
 require 'yaml'
 require 'highline'
 
+
+@host = 'professorships.nd.edu'
+@protocol = 'https'
+# @host = 'localhost:3000'
+# @protocol = 'http'
+
 def get_json_response_for(path)
-  RestClient.get("http://#{File.join('professorships.nd.edu', path).sub(/\/$/, '')}.js", {:params => {'children' => 'true'}, :accept => :json})
+  puts path
+  RestClient.get("#{@protocol}://#{File.join(@host, path).sub(/\/$/, '')}.js", {:params => {'children' => 'true'}, :accept => :json})
 end
 
 def parse_json_for(path)
@@ -27,31 +34,31 @@ parse_json_for('/by-college')['children'].each do |college|
   college_name = college['name'].strip
 
   parse_json_for(college['permalink'])['children'].each do |professorship|
-    chair_name       = professorship['meta_attributes']['chair'].strip
+    professorship_title       = professorship['meta_attributes']['chair'].strip
     prof_name        = professorship['meta_attributes']['name'].strip
     professorship['meta_attributes']['photo'].strip =~ /src="([^"]*)"/
     prof_photo      = $1
-    prof_last_name   = professorship['meta_attributes']['lastnameprof'].strip
-    prof_biography   = professorship['meta_attributes']['bio'].strip
-    chair_last_name  = professorship['meta_attributes']['lastnametitle'].strip
+    professor_last_name   = professorship['meta_attributes']['lastnameprof'].strip
+    biography   = professorship['meta_attributes']['bio'].strip
+    professorship_last_name  = professorship['meta_attributes']['lastnametitle'].strip
     permalink        = professorship['permalink']
-    prof_first_name  = prof_name.sub(prof_last_name, '').strip
+    professor_first_name  = prof_name.sub(professor_last_name, '').strip
     college_id = nil
     unless college_id = @colleges[college_name]
       puts "Warning: no college for #{permalink}"
     end
-    @chairs[chair_name] ||= []
-    @chairs[chair_name] << {
-      'chair_name'      => chair_name,
+    @chairs[professorship_title] ||= []
+    @chairs[professorship_title] << {
+      'professorship_title'      => professorship_title,
       'prof_full_name'  => prof_name,
-      'prof_first_name' => prof_first_name,
-      'prof_last_name'  => prof_last_name,
-      'prof_biography'  => prof_biography,
+      'professor_first_name' => professor_first_name,
+      'professor_last_name'  => professor_last_name,
+      'biography'  => biography,
       'photo_upload'    => prof_photo,
-      'chair_last_name' => chair_last_name,
+      'professorship_last_name' => professorship_last_name,
       'permalink'       => permalink,
       'college_name'    => college_name,
-      'college_id'      => college_id,
+      'college'      => college_id,
       'chair'           => true,
       'directorship'    => false,
     }
@@ -60,27 +67,27 @@ end
 
 # Build the intial collection of directorships
 parse_json_for('/directorships')['children'].each do |directorship|
-  chair_name      = directorship['meta_attributes']['chair'].strip
+  professorship_title      = directorship['meta_attributes']['chair'].strip
   prof_name       = directorship['meta_attributes']['name'].strip
   directorship['meta_attributes']['photo'].strip =~ /src="([^"]*)"/
   prof_photo      = $1
-  prof_last_name  = directorship['meta_attributes']['lastnameprof'].strip
-  prof_biography  = directorship['meta_attributes']['bio'].strip
-  chair_last_name = directorship['meta_attributes']['lastnametitle'].strip
+  professor_last_name  = directorship['meta_attributes']['lastnameprof'].strip
+  biography  = directorship['meta_attributes']['bio'].strip
+  professorship_last_name = directorship['meta_attributes']['lastnametitle'].strip
   permalink       = directorship['permalink']
-  prof_first_name = prof_name.sub(prof_last_name, '').strip
+  professor_first_name = prof_name.sub(professor_last_name, '').strip
 
-  @directorships[chair_name] ||= []
-  @directorships[chair_name] << {
-    'chair_name'      => chair_name,
+  @directorships[professorship_title] ||= []
+  @directorships[professorship_title] << {
+    'professorship_title'      => professorship_title,
     'prof_full_name'  => prof_name,
-    'prof_first_name' => prof_first_name,
-    'prof_last_name'  => prof_last_name,
-    'prof_biography'  => prof_biography,
+    'professor_first_name' => professor_first_name,
+    'professor_last_name'  => professor_last_name,
+    'biography'  => biography,
     'photo_upload'    => prof_photo,
-    'chair_last_name' => chair_last_name,
+    'professorship_last_name' => professorship_last_name,
     'permalink'       => permalink,
-    'college_id'      => nil,
+    'college'      => nil,
     'chair'           => false,
     'directorship'    => true,
   }
@@ -98,17 +105,6 @@ File.open(File.join(File.dirname(__FILE__), '..','tmp/directorships.yml'), 'w+')
   file.puts(YAML.dump(@directorships))
 end
 
-@database_id = 6
-@highline = HighLine.new
-
-def net_id
-  @net_id ||= @highline.ask(@highline.color("Net ID: ", :black, :on_yellow))
-end
-
-def password
-  @password ||= @highline.ask(@highline.color("Password: ", :black, :on_yellow)) { |q| q.echo = "*" }
-end
-
 # Merging the data
 @directorships.each do |key, entry|
   if @chairs.has_key?(key)
@@ -120,25 +116,40 @@ end
   end
 end
 
+@database_id = 6
+@highline = HighLine.new
+
+def net_id
+  @net_id ||= @highline.ask(@highline.color("Net ID: ", :black, :on_yellow))
+end
+
+def password
+  @password ||= @highline.ask(@highline.color("Password: ", :black, :on_yellow)) { |q| q.echo = "*" }
+end
+
+
+File.open(File.join(File.dirname(__FILE__), '..','tmp/chairs-for-upload.yml'), 'w+') do |file|
+  file.puts(YAML.dump(@chairs))
+end
+
 net_id
 password
-@host = 'professorships.nd.edu'
-@protocol = 'https'
-@chairs.each do |chair_name, entries|
+@chairs.each do |professorship_title, entries|
   entries.each_with_index do |entry, index|
     begin
       RestClient.post(
       "#{@protocol}://#{@net_id}:#{@password}@#{@host}/admin/data_store_models/#{@database_id}/records",
       {
+        "without_expire" => 'true',
         "record" => {
           'directorship'            => entry['directorship'],
-          'professorship_title'     => entry['chair_name'],
-          'professorship_last_name' => entry['chair_last_name'],
-          'professor_first_name'    => entry['prof_first_name'],
-          'professor_last_name'     => entry['prof_last_name'],
-          'biography'               => entry['prof_biography'],
+          'professorship_title'     => entry['professorship_title'],
+          'professorship_last_name' => entry['professorship_last_name'],
+          'professor_first_name'    => entry['professor_first_name'],
+          'professor_last_name'     => entry['professor_last_name'],
+          'biography'               => entry['biography'],
           'photo_upload'            => entry['photo_upload'],
-          'college'                 => entry['college_id'],
+          'college'                 => entry['college'],
           'sequence'                => index + 1
         }
       }, {:accept => :xml}
